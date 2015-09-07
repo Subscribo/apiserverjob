ace72c6ee9d4f6bb4a920c1a9e6f73a011c29f8c
slavo <slavo2@users.noreply.github.com>
2015-09-07 11:15:51 +0200

    Package Subscribo ApiServerCommon renamed/moved to package Subscribo ApiServerJob

---
+++ src/Subscribo/ApiServerJob/Jobs/Regular/MaintainDeliveries.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Subscribo\ApiServerJob\Jobs\Regular;
+
+use Subscribo\ApiServerJob\Jobs\AbstractJob;
+use Subscribo\ModelCore\Models\Service;
+use Subscribo\ModelCore\Models\Delivery;
+use Subscribo\ModelCore\Models\Realization;
+use Subscribo\Support\DateTimeUtils;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class MaintainDeliveries
+ *
+ * @package Subscribo\ApiServerJob
+ */
+class MaintainDeliveries extends AbstractJob
+{
+    /** @var \Subscribo\ModelCore\Models\Service  */
+    protected $service;
+
+    /**
+     * @param Service $service
+     */
+    public function __construct(Service $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * @param LoggerInterface $log
+     */
+    public function handle(LoggerInterface $log)
+    {
+        $log->notice("Maintaining Deliveries for service '".$this->service->identifier."' started");
+
+        $addedDeliveries = Delivery::autoAdd($this->service);
+
+        foreach ($addedDeliveries as $delivery) {
+            $log->notice('Added delivery starting from '.DateTimeUtils::exportDateTime($delivery->start));
+        }
+        $log->notice("Deliveries added: ". count($addedDeliveries));
+
+        $updated = Delivery::autoAvailable($this->service);
+
+        $log->notice(
+            "Deliveries in which availability for ordering was enabled: ".count($updated['enabled'])
+            .", disabled: ".count($updated['disabled'])
+            .", stayed enabled: ".count($updated['stayedEnabled'])
+            .", stayed disabled: ".count($updated['stayedDisabled'])
+        );
+        $log->notice("Maintaining Deliveries for service '".$this->service->identifier."' finished");
+        $log->notice("Maintaining Product Realizations for service '".$this->service->identifier."' started");
+
+        $products = $this->service->products;
+
+        $restoredRealizationsCount = Realization::restoreInBoundsRealizations($products);
+
+        $log->notice("Realizations restored: ".$restoredRealizationsCount);
+
+        $softDeletedRealizationsCount = Realization::softDeleteOutOfBoundsRealizations($products);
+
+        $log->notice("Realizations soft deleted: ".$softDeletedRealizationsCount);
+
+        $suppliedRealizations = Realization::supplyRealizations($products, $this->service->deliveries);
+
+        foreach ($suppliedRealizations as $realization) {
+                $log->notice(
+                    "Supplied realization '".$realization->identifier
+                    ."' for product '".$realization->product->identifier
+                    ."' for delivery starting from ".DateTimeUtils::exportDateTime($realization->delivery->start)
+                );
+        }
+        $log->notice("Realizations supplied: ".count($suppliedRealizations));
+        $log->notice("Maintaining Product Realizations for service '".$this->service->identifier."' finished");
+    }
+}
+
